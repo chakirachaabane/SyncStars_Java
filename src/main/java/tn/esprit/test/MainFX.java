b8cd98cb5d3e928a17ae4f1fcf110338e1c315aa
Chakira <chakira.chaabane@esprit.tn>
2024-04-08 02:02:56 +0100

    Second commit

---
+++ src/main/java/tn/esprit/test/MainFX.java
@@ -0,0 +1,27 @@
+package tn.esprit.test;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class MainFX extends Application {
+    @Override
+    public void start(Stage primaryStage)  throws  Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/test-view.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("AlignVibe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
+
+
+
